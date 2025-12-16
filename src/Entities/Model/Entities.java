9836ce6616bf2f81d66fed2f0e9aea4978d19813
Mateus <seuemail@exemplo.com>
2025-12-16 09:03:49 -0300

    very bad solution

---
+++ src/Entities/Model/Entities.java
@@ -0,0 +1,36 @@
+package Entities.Model;
+
+import java.util.Date;
+
+public class Entities {
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    public Entities() {
+    }
+    public Entities(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+
+}
